ff6a28e422ebae2ca36e8fb10d08dfcd3e16e995
Blessy <blessysusancheriyan28@gmail.com>
2023-01-15 15:53:08 +0530

    first commit

--- src/main/java/com/Company/Product/SpringBootApplication/ProductApplication.java
+++ src/main/java/com/Company/Product/SpringBootApplication/ProductApplication.java
@@ -26,16 +26,16 @@ public class ProductApplication {
 	@Bean
 
 	public Docket postsApi() {
-		return new Docket(DocumentationType.SWAGGER_2).groupName("Java Techie").apiInfo(apiInfo()).select()
+		return new Docket(DocumentationType.SWAGGER_2).groupName("Bless").apiInfo(apiInfo()).select()
 				.paths(regex("/check.*")).build();
 	}
 
 	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder().title("Book Service")
 				.description("Sample Documentation Generateed Using SWAGGER2 for our Book Rest API")
-				.termsOfServiceUrl("https://www.youtube.com/channel/UCORuRdpN2QTCKnsuEaeK-kQ")
-				.license("Java_Gyan_Mantra License")
-				.licenseUrl("https://www.youtube.com/channel/UCORuRdpN2QTCKnsuEaeK-kQ").version("1.0").build();
+				.termsOfServiceUrl("")
+				.license(")
+				.licenseUrl("").version("1.0").build();
 	}
 	
 	
